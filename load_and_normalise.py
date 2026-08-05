eef67291b9493d891f5e886fcbf3571a6b4d6219
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2026-08-05 12:12:14 +0100

    add functions for loading data

---
+++ load_and_normalise.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from mantid.simpleapi import *
+
+
+def load_and_normalise(sample_run_no: int, normalise_run_no: int, name: str):
+    name = name if name else str(sample_run_no)
+    sample_ws = load_and_process(sample_run_no, name)
+    normalise_ws = load_and_process(normalise_run_no, "normalise")
+    Divide(sample_ws, normalise_ws, OutputWorkspace=f"{name}_normalised")
+
+
+def load_and_process(run_no: int, name: str):
+    filenames = load_ngem_from_INES_run_number(run_no)
+    ws = LoadNGEM(Filename=",".join(filenames), OutputWorkspace=name)
+    ConvertUnits(InputWorkspace=ws, OutputWorkspace=ws, Target="Energy")
+    CropWorkspace(InputWorkspace=ws, OutputWorkspace=ws, XMin=1000, XMax=1000000)
+    Rebin(InputWorkspace=ws, OutputWorkspace=ws, Params="100,-0.05,1e+06")
+    SumSpectra(InputWorkspace=ws, OutputWorkspace=f"{name}_summed")
+    return ws
+
+
+def load_ngem_from_INES_run_number(run_no: int):
+    data_dir = Path(
+        f"//isis.cclrc.ac.uk/Shares/nGEM-Imaging/DATA/INES_2024_05/INES{run_no}"
+    )
+    data_files = data_dir.glob("**/*.edb")
+    return [str(f) for f in data_files]
+
+
+load_and_normalise(24981, 24980, "O3_4")
