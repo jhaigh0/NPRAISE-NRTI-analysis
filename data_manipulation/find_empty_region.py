50e9fe73be731ebf553b5d6f8161141faf7f2c1a
Jonathan Haigh <jonathan.haigh@stfc.ac.uk>
2026-08-28 11:09:20 +0100

    add function to get dimensions of ROI

--- data_manipulation/find_empty_region.py
+++ data_manipulation/find_empty_region.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn.decomposition import PCA
 
+from data_manipulation.mix_workspaces import get_grid_dimensions
+
 
 def generate_threshhold_mask_percentage(ws, threshold: float):
     nspec = ws.getNumberHistograms()
@@ -30,3 +32,14 @@ def perform_pca_on_workspace(ws, n_components):
     y_data = np.nan_to_num(ws.extractY(), nan=0.0)
     scores = pca.fit_transform(y_data)
     return scores, pca
+
+
+def get_dimensions_of_roi_workspace(ws):
+    nrows, ncols = get_grid_dimensions(ws)
+    all_detectors = np.arange(1, nrows * ncols + 1).reshape(nrows, ncols)
+    det_ids = list(ws.getDetectorIDToWorkspaceIndexMap(True, True).keys())
+    mask = np.isin(all_detectors, det_ids)
+    rows, cols = np.where(mask)
+    height = rows.max() - rows.min() + 1
+    width = cols.max() - cols.min() + 1
+    return height, width
